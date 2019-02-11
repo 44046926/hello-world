039df1751244ea901e04bc13bfcb695fd1512ef3
gitkraken <642849025@qq.com>
2019-02-11 12:39:12 +0800

    c

--- src/main/java/com/market/controller/HelloWorldController.java
+++ src/main/java/com/market/controller/HelloWorldController.java
@@ -33,6 +33,7 @@ public class HelloWorldController {
         logger.warn("=======================warn");
         logger.warn("=======================a");
         logger.warn("=======================b");
+        logger.warn("=======================c");
         return "login: " + id + ", " + name + ", path: " + logInfo.getPath();
     }
 
